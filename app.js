b3ccff8fb8a5c6a7f1c76be1dcc4282181b4152f
Randeep Singh <rand-singh@users.noreply.github.com>
2020-08-08 12:33:36 +0100

    Generate obstacle and animate towards dino
    Detect collision and show game over message

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const dino = document.querySelector('.dino');
+const grid = document.querySelector('.grid');
+const alert = document.getElementById('alert');
 let isJumping = false;
 let gravity = 0.9;
+let isGameOver = false;
 
 function control(e) {
     if (e.keyCode === 32) {
@@ -44,3 +47,27 @@ function jump() {
         console.log(dino.style.bottom)
     }, 20);
 }
+
+function generateObstacles() {
+    let randomTime = Math.random() * 4000;
+    let obstaclePosition = 1000;
+    const obstacle = document.createElement('div');
+    if (!isGameOver) obstacle.classList.add('obstacle');
+
+    grid.appendChild(obstacle);
+    obstacle.style.left = obstaclePosition + 'px';
+
+    let timerId = setInterval(function() {
+        if(obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
+            clearInterval(timerId);
+            alert.innerHTML = 'Game Over';
+            isGameOver = true;
+
+        }
+
+        obstaclePosition -=10;
+        obstacle.style.left = obstaclePosition + 'px';
+    }, 20);
+    if (!isGameOver) setTimeout(generateObstacles, randomTime);
+}
+generateObstacles();
